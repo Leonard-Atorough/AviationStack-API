cfba503e37c88407901e72c3c1880a48b6e63ede
Leonard Atorough <leotorough@gmail.com>
2021-01-08 14:21:38 +0000

    Fixed namespaces

--- AviationStack/AviationStack/Tests/AvStackAirplaneTests.cs
+++ AviationStack/AviationStack/Tests/AvStackAirplaneTests.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AviationStack.Tests
+namespace AviationStack
 {
     class AvStackAirplaneTests
     {
+        [Test]
     }
 }
